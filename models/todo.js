80eca29c7b202add3af4a4b3d24769f4a3bc8f6c
thaynar96 <thaynarbo@gmail.com>
2021-09-13 21:06:34 -0300

    changed dataCriacao

--- models/todo.js
+++ models/todo.js
@@ -1,17 +1,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const TodoSchema = new Schema({
-	titulo: { type: String, required: true },
-	descricao: { type: String, required: true },
-	status: { type: String, default: 'fazer' },
-	prioridade: { type: Number, default: 1 },
-	prazo: { type: String },
-	dataCriacao: {
-		type: String,
-		default: new Date().toLocaleDateString('pt-BR'),
+const TodoSchema = new Schema(
+	{
+		titulo: { type: String, required: true },
+		descricao: { type: String, required: true },
+		status: { type: String, default: 'fazer' },
+		prioridade: { type: Number, default: 1 },
+		prazo: { type: String },
 	},
-});
+	{ timestamps: true }
+);
 
 const Todo = mongoose.model('Todo', TodoSchema);
 
